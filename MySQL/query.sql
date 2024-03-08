f36a782248ec81219032ea9942155f706531bc9a
Humberto Silva <humbertot@hotmail.com.br>
2024-03-08 07:54:30 -0300

    .

--- MySQL/query.sql
+++ MySQL/query.sql
@@ -1,4 +1,3 @@
-/*
 create database bot_discord;
 
 create table servidor(
@@ -14,24 +13,3 @@ nome varchar(45) not null,
 fk_id_servidor varchar(20) unique,
 foreign key (fk_id_servidor) references servidor(id_servidor)
 );
-
-create table streamers(
-id_streamer varchar(20) primary key,
-nome varchar(45) not null
-);
-
-create table servidor_streamers(
-id_servidor_streamer int primary key auto_increment,
-fk_id_streamer varchar(20),
-fk_id_servidor varchar(20),
-foreign key (fk_id_streamer) references streamers(id_streamer),
-foreign key (fk_id_servidor) references servidor(id_servidor)
-);
-
-create table canal_twitch(
-id_canal_twitch varchar(20) primary key,
-nome varchar(45) not null,
-fk_id_servidor varchar(20),
-foreign key (fk_id_servidor) references servidor(id_servidor)
-);
-*/
